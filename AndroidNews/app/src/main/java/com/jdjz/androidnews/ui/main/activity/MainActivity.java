54619e3fa0dae1245f04ffed12524c90e7e49240
whtchl <whtchl@126.com>
2016-10-31 21:23:20 +0800

    MainActivity

--- AndroidNews/app/src/main/java/com/jdjz/androidnews/ui/main/activity/MainActivity.java
+++ AndroidNews/app/src/main/java/com/jdjz/androidnews/ui/main/activity/MainActivity.java
@@ -1,17 +1,23 @@
 package com.jdjz.androidnews.ui.main.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.flyco.tablayout.CommonTabLayout;
 import com.jdjz.androidnews.R;
 import com.jdjz.common.base.BaseActivity;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
+    @Bind(R.id.tab_layout)
+    CommonTabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+        ButterKnife.bind(this);
     }
 
     @Override
